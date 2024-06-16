71acb3c37d1e9efbc5fffaa86b7db596ccb152eb
yuanze31 <1538981620@qq.com>
2024-06-16 15:33:45 +0800

    v1.1.0

---
+++ edit.py
@@ -0,0 +1,108 @@
+﻿import os
+import subprocess
+import time
+import xml.etree.ElementTree as ET
+
+
+def edit_gamename(gamename):
+    gamename_status = "不修改服务器名称"
+    WindowsUsername = os.getlogin()
+    if gamename != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for game_name in root.iter("game_name"):
+            game_name.text = str(gamename)
+        tree.write(config_path)
+        gamename_status = "服务器名称：" + gamename + "\n"
+    return gamename_status
+
+
+def edit_gamepswd(gamepswd):
+    gamepswd_status = "不修改服务器密码"
+    WindowsUsername = os.getlogin()
+    if gamepswd != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for game_password in root.iter("game_password"):
+            game_password.text = str(gamepswd)
+        tree.write(config_path)
+        gamepswd_status = "服务器密码：" + gamepswd + "\n"
+    return gamepswd_status
+
+
+def edit_gameport(gameport):
+    gameport_status = "不修改服务器端口"
+    WindowsUsername = os.getlogin()
+    if gameport != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for port in root.iter("port"):
+            port.text = str(gameport)
+        tree.write(config_path)
+        gameport_status = "服务器名称：" + gameport + "\n"
+    return gameport_status
+
+
+def edit_gameslot(gameslot):
+    gameslot_status = "不修改存档槽"
+    WindowsUsername = os.getlogin()
+    if gameslot != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for savegame_index in root.iter("savegame_index"):
+            savegame_index.text = str(gameslot)
+        tree.write(config_path)
+        gameslot_status = "存档槽：" + gameslot + "\n"
+    return gameslot_status
+
+
+def edit_maxplayer(maxplayer):
+    maxplayer_status = "不修改最大人数"
+    WindowsUsername = os.getlogin()
+    if maxplayer != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for max_player in root.iter("max_player"):
+            max_player.text = str(maxplayer)
+        tree.write(config_path)
+        maxplayer_status = "最大人数：" + maxplayer + "\n"
+    return maxplayer_status
+
+
+def edit_autosaveinterval(autosaveinterval):
+    autosaveinterval_status = "不修改自动保存间隔"
+    WindowsUsername = os.getlogin()
+    if autosaveinterval != "":
+        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
+        tree = ET.parse(config_path)
+        root = tree.getroot()
+        for auto_save_interval in root.iter("auto_save_interval"):
+            auto_save_interval.text = str(autosaveinterval)
+        tree.write(config_path)
+        autosaveinterval_status = "自动保存间隔：" + autosaveinterval + "\n"
+    return autosaveinterval_status
+
+
+def start_game():
+    WindowsUsername = os.getlogin()
+    print("即将在3秒后启动，使用 Ctrl + C 取消")
+    time.sleep(3)
+    print("启动中...")
+    # Steam路径
+    steam_path = "C:\\Program Files (x86)\\Steam\\Steam.exe"
+    # 游戏ID
+    game_id = "1248130"
+    # 启动参数
+    launch_options = (
+        '-name FS22 -profile "C:/Users/'
+        + WindowsUsername
+        + '/Documents/My Games/FarmingSimulator2022" -server'
+    )
+
+    command = f'"{steam_path}" -applaunch {game_id} {launch_options}'
+    subprocess.run(command)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # date: 2024-06-16
 # author: yuanze31
-# version: 1.0.0
+# version: 1.1.0
 #
 # ——--————/´ ¯/)
 # —— ———--/—-/
@@ -16,88 +16,69 @@
 # —— ——-\———-\
 
 import os
-import subprocess
-import sys
-import time
 import xml.etree.ElementTree as ET
 
-import keyboard
+import edit
 
 # 初始化
-WindowsUsername = os.getlogin()
-gamename_status = "服务器名称：不修改\n"
-gameslot_status = "存档槽：不修改\n"
 
 
-def edit_gamename():
-    global gamename_status
-    print("更改game_name，回车不修改直接启动\n请勿输入无关字符")
-    gamename = input()
-    if gamename != "":
-        # 构建文件路径
-        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
-        # 解析XML文件
-        tree = ET.parse(config_path)
-        root = tree.getroot()
-        # 更新<game_name>
-        for game_name in root.iter("game_name"):
-            game_name.text = str(gamename)
-        # 保存
-        tree.write(config_path)
-        gamename_status = "服务器名称：" + gamename + "\n"
-
-
-def edit_gameslot():
-    global gameslot_status
-    print("更改gameslot，回车不修改直接启动\n请勿输入无关字符")
-    gameslot = input()
-    if gameslot != "":
-        # 构建文件路径
-        config_path = f"C:/Users/{WindowsUsername}/Documents/My Games/FarmingSimulator2022/dedicated_server/dedicatedServerConfig.xml"
-        # 解析XML文件
-        tree = ET.parse(config_path)
-        root = tree.getroot()
-        # 更新<savegame_index>
-        for savegame_index in root.iter("savegame_index"):
-            savegame_index.text = str(gameslot)
-        # 保存
-        tree.write(config_path)
-        gameslot_status = "存档槽：" + gameslot + "\n"
-
-
-def start_game():
-    print("即将在3秒后启动，使用 Ctrl + C 取消")
-    time.sleep(3)
-    print("启动中...")
-    # Steam路径
-    steam_path = "C:\\Program Files (x86)\\Steam\\Steam.exe"
-    # 游戏ID
-    game_id = "1248130"
-    # 启动参数
-    launch_options = '-name FS22 -profile "C:/Users/15389/Documents/My Games\\FarmingSimulator2022" -server'
-
-    command = f'"{steam_path}" -applaunch {game_id} {launch_options}'
-    subprocess.run(command)
-
-
-print("是否快速启动？\n回车-是   N-否")
-fast_start = input()
-if fast_start == "":
-    start_game()
-    sys.exit(0)
-else:
-    edit_gamename()
-    edit_gameslot()
-
-print("游戏即将启动，请验证以下参数\n" + gamename_status + gameslot_status)
-print("输入Y确认启动")
-will_start = input()
-if will_start == "Y" or will_start == "y":
-    start_game()
-else:
-    print("取消启动")
-    print("按任意键退出...")
+def menu():
     while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN:
+        os.system("cls")
+        print("--------------")
+        print("FUCK YOU GIANT")
+        print("--------------")
+        print("FS22 便捷服务器面板 V1.1.0\n")
+        print("1 - 启动服务器")
+        print("2 - 修改服务器名称")
+        print("3 - 修改服务器端口")
+        print("4 - 修改游戏密码")
+        print("5 - 修改存档槽位")
+        print("6 - 修改最大人数")
+        print("7 - 修改自动保存间隔")
+        print("99 - 退出")
+
+        choice = input("请输入你的选择: ")
+        if choice == "1":  # 启动服务器
+            os.system("cls")
+            edit.start_game()
             break
+        elif choice == "2":  # 修改服务器名称
+            os.system("cls")
+            print("更改服务器名称（非中文），回车不修改\n请勿输入无关字符")
+            gamename = input()
+            print(edit.edit_gamename(gamename))
+        elif choice == "3":  # 修改游戏端口
+            os.system("cls")
+            print("更改服务器端口（纯数字），回车不修改\n请勿输入无关字符")
+            gameport = input()
+            print(edit.edit_gameport(gameport))
+        elif choice == "4":  # 修改游戏密码
+            os.system("cls")
+            print("更改服务器密码（非中文），回车不修改\n请勿输入无关字符")
+            gamepswd = input()
+            print(edit.edit_gamepswd(gamepswd))
+        elif choice == "5":  # 修改存档槽位
+            os.system("cls")
+            print("更改使用的存档槽（纯数字），回车不修改\n请勿输入无关字符")
+            gameslot = input()
+            print(edit.edit_gameslot(gameslot))
+        elif choice == "6":  # 修改最大人数
+            os.system("cls")
+            print("更改最大人数（纯数字），回车不修改\n请勿输入无关字符")
+            maxplayer = input()
+            print(edit.edit_maxplayer(maxplayer))
+        elif choice == "7":  # 修改自动保存间隔
+            os.system("cls")
+            print("修改自动保存间隔（纯数字、秒），回车不修改\n请勿输入无关字符")
+            auto_save_interval = input()
+            print(edit.edit_autosaveinterval(auto_save_interval))
+        elif choice == "99":  # 退出
+            break
+        else:
+            os.system("cls")
+
+
+if __name__ == "__main__":
+    menu()
